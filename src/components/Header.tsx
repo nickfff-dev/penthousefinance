963de074b78a74f825eca6b5fe1147adf7a528a2
unknown <hunchographics@gmail.com>
2022-11-21 15:50:54 +0300

    darkmode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-
+import SwitchDarkMode from "./SwitchDarkMode"
 import { useEffect, useState } from "react";
 
 
@@ -51,29 +51,30 @@ const stickyOnScroll = () => {
     console.log(show)
   }
   return (
-    <nav className="bg-primary  border-b border-black" id="navbar">
+    <nav className="bg-primary dark:bg-black dark:text-secondary  border-b border-black" id="navbar">
 
       <div className="md:mx-5 flex items-center justify-between p-4 ">
         <div className="flex md:order-3 order-2 justify-between">
-          <button type="button" className="text-black hover:text-secondary shadow uppercase md:text-lg bg-secondary hover:bg-black focus:ring-4 md:mr-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-1.5 text-center  md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Join Us</button>
+          <button type="button" className="text-black hover:text-primary  shadow uppercase md:text-lg bg-secondary hover:bg-black focus:ring-4 md:mr-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-1.5 text-center  md:mr-0 dark:bg-secondary  ">Join Us</button>
           <button data-collapse-toggle="navbar-cta" onClick={() => {
            toggleMenu();
           }} type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-8 h-8" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           </button>
+          <SwitchDarkMode/>
         </div>
         <a href="https://flowbite.com/" className=" md:-ml-20  md:order-2 items-start ">
-       <img src="https://penthouse.finance/wp-content/uploads/2022/05/Penthouse_banner1-300x48.png" className="h-6 mr-3 sm:h-12" alt="Flowbite Logo" />
+       <img src="https://penthouse.finance/wp-content/uploads/2022/05/Penthouse_banner1-300x48.png" className="h-6 mr-3 sm:h-12 dark:bg-secondary dark:p-1" alt="Flowbite Logo" />
      
          </a>
         <div className={`${show ? "block" : "hidden"} items-center justify-between  w-full md:flex md:w-auto md:block md:order-1`} id="cta">
-          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8  md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8  md:mt-0 md:text-sm md:font-medium md:border-0   ">
             <li>
-              <a href="/" className="block  text-lg uppercase  text-gray-700  rounded md:bg-transparent  md:p-0 dark:text-white" aria-current="page">How It Works</a>
+              <a href="/" className="block  text-lg uppercase  text-gray-700  rounded md:bg-transparent  md:p-0 dark:text-secondary" aria-current="page">How It Works</a>
             </li>
             <li>
-              <a href="/" className="block  text-lg   uppercase text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
+              <a href="/" className="block  text-lg   uppercase text-gray-700 rounded    md:p-0  dark:text-secondary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
             </li>
           
           </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}", "./public/index.html",'./src/components/**/*.{js,jsx,ts,tsx}','./src/pages/**/*.{js,jsx,ts,tsx}',],
+  content: ["./src/**/*.{html,js,ts,tsx}", "./public/index.html", './src/components/**/*.{js,jsx,ts,tsx}', './src/pages/**/*.{js,jsx,ts,tsx}',],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
