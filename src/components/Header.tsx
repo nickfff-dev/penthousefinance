ecb984ae9fc5873b5e681eb3fdee23993a9994eb
unknown <hunchographics@gmail.com>
2022-11-21 17:09:54 +0300

    changed colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -51,11 +51,11 @@ const stickyOnScroll = () => {
     console.log(show)
   }
   return (
-    <nav className="bg-primary dark:bg-black dark:text-secondary  border-b border-black dark:border-secondary" id="navbar">
+    <nav className="bg-primary dark:bg-black border-b border-black dark:border-gray-200" id="navbar">
 
       <div className="md:mx-5 flex items-center justify-between p-4 ">
         <div className="flex md:order-3 order-2 justify-between">
-          <button type="button" className="text-black hover:text-primary  shadow uppercase md:text-lg bg-secondary hover:bg-black focus:ring-4 md:mr-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-1.5 text-center  md:mr-0 dark:bg-secondary  ">Join Us</button>
+          <button type="button" className="text-black dark:text-primary dark:hover:text-black  shadow uppercase md:text-lg bg-secondary hover:bg-black focus:ring-4 md:mr-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-1.5 text-center  md:mr-0 dark:bg-secondary  ">Join Us</button>
           <button data-collapse-toggle="navbar-cta" onClick={() => {
            toggleMenu();
           }} type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
@@ -65,16 +65,16 @@ const stickyOnScroll = () => {
           <SwitchDarkMode/>
         </div>
         <a href="https://flowbite.com/" className=" md:-ml-20  md:order-2 items-start ">
-       <img src="https://penthouse.finance/wp-content/uploads/2022/05/Penthouse_banner1-300x48.png" className="h-6 mr-3 sm:h-12 dark:bg-secondary dark:p-1" alt="Flowbite Logo" />
+       <img src="https://penthouse.finance/wp-content/uploads/2022/05/Penthouse_banner1-300x48.png" className="h-6 mr-3 sm:h-12 dark:bg-white dark:p-1" alt="Flowbite Logo" />
      
          </a>
         <div className={`${show ? "block" : "hidden"} items-center justify-between  w-full md:flex md:w-auto md:block md:order-1`} id="cta">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8  md:mt-0 md:text-sm md:font-medium md:border-0   ">
             <li>
-              <a href="/" className="block  text-lg uppercase  text-gray-700  rounded md:bg-transparent  md:p-0 dark:text-secondary" aria-current="page">How It Works</a>
+              <a href="/" className="block  text-lg uppercase  text-gray-700  rounded   md:p-0 dark:text-white dark:hover:text-secondary" aria-current="page">How It Works</a>
             </li>
             <li>
-              <a href="/" className="block  text-lg   uppercase text-gray-700 rounded    md:p-0  dark:text-secondary dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About Us</a>
+              <a href="/" className="block  text-lg   uppercase text-gray-700 rounded    md:p-0  dark:text-white  dark:hover:text-secondary ">About Us</a>
             </li>
           
           </ul>
--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -5,33 +5,33 @@ import mine  from "../images/hero.jpeg"
 
 const Herosection = () => { 
   return (
-  <div className="grid  grid-flow-row bg-primary dark:bg-black grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 gap-4 border-b dark:border-secondary border-black">
+  <div className="grid  grid-flow-row bg-primary dark:bg-black grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 gap-4 border-b dark:border-gray-200 border-black">
   
   <div className="flex items-start justify-center">
     <main className="max-w-screen-xl p-4 lg:px-16">
         <div className="text-left flex-col space-y-10">
-          <h2 className="text-5xl    text-secondary  sm:leading-none ">
+          <h2 className="text-5xl    text-secondary dark:text-yellow-300    sm:leading-none ">
               GENERATE PASSIVE INCOME FROM
        
             </h2>
-            <h2 className="text-5xl tracking-tight    text-secondary  sm:leading-none ">
+            <h2 className="text-5xl tracking-tight   text-secondary dark:text-yellow-300   sm:leading-none ">
               
               HIGH RETURN RENTAL PROPERTIES 
             
        
             </h2>
-            <h2 className="text-5xl tracking-tight    text-secondary  sm:leading-none">
+            <h2 className="text-5xl tracking-tight  dark:text-yellow-300   text-secondary  sm:leading-none">
               
             AT A FRACTION OF THE COST
             
        
           </h2>
-          <p className="mt-3 dark:text-gray-300 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+          <p className="mt-3 dark:text-gray-200 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           Penthouse allows you to co-invest in high return holiday rental properties that generate attractive returns at 10 times less than the normal cost: all 100% hands-off. Get the stability and yield of luxury real estate with the convenience of NFTs.
           </p>
           <div className="mt-5 sm:mt-8 sm:flex justify-start">
             <div className="rounded-full shadow">
-              <a href="/" className="w-full flex items-center justify-center px-8 py-3 uppercase text-base leading-6 font-medium rounded-full text-white bg-secondary hover:bg-black hover:text-secondary focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 dark:text-black">
+              <a href="/" className="w-full flex items-center justify-center px-8 py-3 uppercase text-base leading-6 font-medium rounded-full text-white bg-secondary hover:bg-black hover:text-secondary focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-3 md:text-lg md:px-10 dark:text-white">
                 Reserve Your Seat
               </a>
             </div>
