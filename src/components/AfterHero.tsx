557cdcd5c803287212d9b28ca02c1c82e8bf2f9f
unknown <hunchographics@gmail.com>
2022-11-22 16:33:51 +0300

    hero change

--- src/components/AfterHero.tsx
+++ src/components/AfterHero.tsx
@@ -6,7 +6,7 @@ const AfterHero = () => {
     <div className="m-5 p-4 rounded-3xl dark:bg-black bg-primary">
       <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-5 p-6 items-start mb-3 ">
       <p className="px-8 lg:text-lg dark:text-gray-300 text-sm">Penthouse is for people who want to invest in vacation rental properties – but don’t want to buy a whole home, don’t have the financial capability to buy an entire property, and/or don’t want to deal with the headaches that go along with it.</p>
-      <p className="px-8 lg:text-lg dark:text-gray-300" >We analyse thousands of premium holiday rental properties to pick out the ones with the most upside. We manage everything for you, from finding tenants to handling repairs. And best of all, we make it easy for you to get started in just a few minutes.</p>
+      <p className="px-8 lg:text-lg dark:text-gray-300 text-sm" >We analyse thousands of premium holiday rental properties to pick out the ones with the most upside. We manage everything for you, from finding tenants to handling repairs. And best of all, we make it easy for you to get started in just a few minutes.</p>
       </div>
 <div className="flex flex-col justify-center items-center">      <a href="/" className=" px-8 py-3  uppercase dark:text-black text-base leading-6 font-medium rounded-full text-white bg-secondary dark:bg-yellow-200 dark:hover:bg-secondary hover:text-black focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
               Get Started
