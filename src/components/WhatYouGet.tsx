cafa8a0d73fe5a119dc277cd2a3bd168d1e9dee2
unknown <hunchographics@gmail.com>
2022-11-22 17:04:34 +0300

    mobile fix

--- src/components/WhatYouGet.tsx
+++ src/components/WhatYouGet.tsx
@@ -3,60 +3,60 @@
 
 
 
-const WhatYouGet = () => { 
+const WhatYouGet = () => {
   return (
-  <div className="m-5 rounded-3xl p-6 grid relative   grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 bg-primary dark:bg-black    dark:border-gray-200 border-black">
-     <div className="ml-12 pt-5 pb-5 "  >
-        <img src="https://penthouse.finance/wp-content/uploads/2022/05/6276f994883474700c245a6b_spacejoy-3z_61bnbFhM-unsplash-p-800.jpeg" className="rounded-3xl max-w-full h-auto" alt="hero"/>
-  </div>
+    <div className="m-5 rounded-3xl p-6 grid grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 bg-primary dark:bg-black    dark:border-gray-200 border-black">
+      <div className="pt-5 mx-auto pb-5"  >
+        <img src="https://penthouse.finance/wp-content/uploads/2022/05/6276f994883474700c245a6b_spacejoy-3z_61bnbFhM-unsplash-p-800.jpeg" className="rounded-3xl max-w-full h-auto" alt="hero" />
+      </div>
       <div className="p-4 space-x-4 space-y-4">
-        <h1 className=" lg:text-4xl text-center w-full mb-8 dark:text-yellow-100">WITH PENTHOUSE, YOU GET:</h1>
-      <div className="flex flex-col  items-start justify-between gap-3 relative">
-      
-        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-6 items-start">
-          <div className="lg:text-left sm:text-center p-4">
-            <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300">Consistent Passive Income</h3>
-<p className="dark:text-primary text-sm leading-relaxed">Earn rental income and receive deposits quarterly. Penthouse acquires and manages your properties so you don’t have to do anything.</p>
+        <h1 className=" text-4xl text-center w-full mb-8 dark:text-yellow-100">WITH PENTHOUSE, YOU GET:</h1>
+        <div className="flex flex-col justify-between gap-3 relative">
+
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1  gap-6">
+            <div className="p-4">
+              <h3 className="lg:text-xl lg:text-left sm:text-center text-center md:text-center font-semibold mb-3 dark:text-gray-300">Consistent Passive Income</h3>
+              <p className="dark:text-primary text-sm  leading-relaxed text-center lg:text-left sm:text-center md:text-center">Earn rental income and receive deposits quarterly. Penthouse acquires and manages your properties so you don’t have to do anything.</p>
+            </div>
+            <div className="p-4">
+              <h3 className="lg:text-xl lg:text-left sm:text-center text-center md:text-center font-semibold mb-3 dark:text-gray-300  ">High-Yield</h3>
+              <p className="dark:text-primary text-sm leading-relaxed text-center lg:text-left sm:text-center md:text-center">Our properties are located in stable high appreciation and high cash-flowing vacation markets that deliver a strong long-term return.</p>
+            </div>
           </div>
-          <div className="lg:text-left sm:text-center p-4">
-            <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300">High-Yield</h3>
-<p className="dark:text-primary text-sm leading-relaxed">Our properties are located in stable high appreciation and high cash-flowing vacation markets that deliver a strong long-term return.</p>
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-5 ">
+            <div className=" p-4">
+              <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300 text-center lg:text-left sm:text-center md:text-center">Fractional Ownership</h3>
+              <p className="dark:text-primary text-sm leading-relaxed text-center lg:text-left sm:text-center md:text-center">Shares are offered in select vacation rentals – this results in a lower barrier to entry and lower cost of diversification.</p>
+            </div>
+            <div className="p-4">
+              <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300 text-center lg:text-left sm:text-center md:text-center">Modern Technology, Traditional Finance</h3>
+              <p className="dark:text-primary text-sm leading-relaxed text-center lg:text-left sm:text-center md:text-center">Gain the advantage of Non-Fungible Token (NFT) technology while keeping the yields and consistency of real estate.</p>
+            </div>
           </div>
-        </div>
-        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-5 items-start">
-          <div className="lg:text-left sm:text-center p-4">
-            <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300">Fractional Ownership</h3>
-<p className="dark:text-primary text-sm leading-relaxed">Shares are offered in select vacation rentals – this results in a lower barrier to entry and lower cost of diversification.</p>
+
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-5 ">
+            <div className=" p-4">
+              <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300 text-center lg:text-left sm:text-center md:text-center">Diversify with Real Estate</h3>
+              <p className="dark:text-primary text-sm leading-relaxed text-center lg:text-left sm:text-center md:text-center" >Access historically consistent returns with low correlation to either the crypto market or the stock market.</p>
+            </div>
+            <div className="p-4">
+              <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300 text-center lg:text-left sm:text-center md:text-center ">Flexible Investment Amounts</h3>
+              <p className="dark:text-primary text-sm leading-relaxed text-center lg:text-left md:text-center sm:text-center ">Invest anywhere from 100 USDC to 20,000 USDC per house and build a portfolio across several properties.</p>
+            </div>
           </div>
-          <div className="lg:text-left sm:text-center p-4">
-            <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300">Modern Technology, Traditional Finance</h3>
-<p className="dark:text-primary text-sm leading-relaxed">Gain the advantage of Non-Fungible Token (NFT) technology while keeping the yields and consistency of real estate.</p>
+          <div className="lg:mx-0 sm:mx-auto lg:pl-4"> <a href="/" className="uppercase  dark:hover:bg-secondary flex items-center justify-center px-5 py-3  text-base leading-6 font-medium rounded-full text-white dark:text-black hover:text-black bg-secondary dark:bg-yellow-200  dark:hover:text-black focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-3 md:text-lg md:px-10 ">
+            Learn More
+          </a>
           </div>
+
         </div>
 
-        <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-5 items-start">
-          <div className="lg:text-left sm:text-center p-4">
-            <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300">Diversify with Real Estate</h3>
-<p className="dark:text-primary text-sm leading-relaxed" >Access historically consistent returns with low correlation to either the crypto market or the stock market.</p>
-          </div>
-          <div className="lg:text-left sm:text-center p-4">
-            <h3 className="lg:text-xl font-semibold mb-3 dark:text-gray-300">Flexible Investment Amounts</h3>
-<p className="dark:text-primary text-sm leading-relaxed">Invest anywhere from 100 USDC to 20,000 USDC per house and build a portfolio across several properties.</p>
-          </div>
-        </div>
-       <div className="lg:mx-0 sm:mx-auto lg:pl-4"> <a href="/" className="uppercase  dark:hover:bg-secondary flex items-center justify-center px-5 py-3  text-base leading-6 font-medium rounded-full text-white dark:text-black hover:text-black bg-secondary dark:bg-yellow-200  dark:hover:text-black focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-3 md:text-lg md:px-10 ">
-                Learn More
-              </a>
- </div>
+      </div>
 
-        </div>
-      
-   </div>
-   
-  
-</div>
 
- )
+    </div>
+
+  )
 }
 
 
