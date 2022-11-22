eed2c38aaa046c9546bdafe64fe59c03b19c6cd4
unknown <hunchographics@gmail.com>
2022-11-22 17:44:07 +0300

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,8 +53,8 @@ const stickyOnScroll = () => {
   return (
     <nav className="z-10 bg-primary dark:bg-black border-b border-yellow-100 " id="navbar">
 
-      <div className="md:mx-5 flex items-center justify-between p-4 ">
-        <div className="flex md:order-3 order-2 justify-between">
+      <div className="flex items-center justify-between p-4 ">
+        <div className="flex  md:order-3 order-2">
           <button type="button" className="text-black dark:text-black dark:hover:text-black  shadow uppercase md:text-lg bg-secondary dark:bg-yellow-200 hover:text-white  dark:hover:bg-secondary focus:ring-4 md:mr-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-6 py-1.5 text-center  md:mr-0 dark:bg-secondary  ">Join Us</button>
           <button data-collapse-toggle="navbar-cta" onClick={() => {
            toggleMenu();
@@ -64,17 +64,17 @@ const stickyOnScroll = () => {
           </button>
           <SwitchDarkMode/>
         </div>
-        <a href="https://flowbite.com/" className=" md:-ml-20  md:order-2 items-start ">
-       <img src="https://penthouse.finance/wp-content/uploads/2022/05/Penthouse_banner1-300x48.png" className="h-6 mr-3 sm:h-12 dark:bg-white dark:p-1" alt="Flowbite Logo" />
+     <div className="md:order-2 md:pl-5  md:-ml-7">   <a href="https://flowbite.com/" >
+       <img src="https://penthouse.finance/wp-content/uploads/2022/05/Penthouse_banner1-300x48.png" className=" dark:bg-white dark:p-1" alt="Flowbite Logo" />
      
-         </a>
-        <div className={`${show ? "block" : "hidden"} items-center justify-between  w-full md:flex md:w-auto md:block md:order-1`} id="cta">
+         </a></div>
+        <div className={`${show ? "block" : "hidden"} items-center justify-between   md:flex  md:block md:order-1`} id="cta">
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8  md:mt-0 md:text-sm md:font-medium md:border-0   ">
             <li>
-              <a href="/" className="block  text-lg uppercase  text-gray-700  rounded   md:p-0 dark:text-white dark:hover:text-secondary" aria-current="page">How It Works</a>
+              <a href="/" className="block  lg:text-lg uppercase  text-gray-700  rounded   md:p-0 dark:text-white dark:hover:text-secondary" aria-current="page">How It Works</a>
             </li>
             <li>
-              <a href="/" className="block  text-lg   uppercase text-gray-700 rounded    md:p-0  dark:text-white  dark:hover:text-secondary ">About Us</a>
+              <a href="/" className="block  lg:text-lg   uppercase text-gray-700 rounded    md:p-0  dark:text-white  dark:hover:text-secondary ">About Us</a>
             </li>
           
           </ul>
