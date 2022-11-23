b5b3eff6bddd39879a96744ed13a8bd8b29b84d2
unknown <hunchographics@gmail.com>
2022-11-23 23:50:57 +0300

    priivacy

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -75,7 +75,7 @@ const AboutUs = () => {
       <div className="max-w-4xl flex items-center h-auto  flex-wrap mx-auto my-10 lg:my-0">
     
    
-    <div id="profile" className="w-full lg:w-3/5  shadow-2xl bg-white opacity-75 mx-6 lg:mx-0 dark:opacity-100">
+    <div id="profile" className="w-full lg:w-3/5  shadow-2xl bg-primary opacity-75 mx-6 lg:mx-0 dark:opacity-100">
     
   
       <div className="p-4 md:p-12 text-center lg:text-left ">
@@ -118,7 +118,7 @@ const AboutUs = () => {
     
     
   </div>
-  <div id="profile" className="w-full lg:w-3/5  shadow-2xl bg-white opacity-75 dark:opacity-100 mx-6 lg:mx-0">
+  <div id="profile" className="w-full lg:w-3/5  shadow-2xl bg-primary opacity-75 dark:opacity-100 mx-6 lg:mx-0">
   
 
     <div className="p-4 md:p-12 text-center lg:text-left">
@@ -151,7 +151,7 @@ const AboutUs = () => {
       <div className="max-w-4xl flex items-center h-auto  flex-wrap mx-auto my-32 lg:my-0 mb-3">
     
 
-  <div id="profile" className="w-full lg:w-3/5   shadow-2xl bg-white dark:opacity-100 mx-6 lg:mx-0">
+  <div id="profile" className="w-full lg:w-3/5   shadow-2xl bg-primary dark:opacity-100 mx-6 lg:mx-0">
   
 
     <div className="p-4 md:p-12 text-center lg:text-left">
